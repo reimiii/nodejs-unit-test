0ce0d8eaac2f37a8b8c8a1a4608f5757a4157bc0
Hilmi AM <imiia75775@proton.me>
2024-09-05 02:29:30 +0700

    jest config

--- test/sum.test.js
+++ test/sum.test.js
@@ -5,3 +5,13 @@ test("test sum", () => {
 
     expect(res).toBe(3)
 })
+test("test sum 2", () => {
+    const res = sum(1, 2);
+
+    expect(res).toBe(3)
+})
+test("test sum 3", () => {
+    const res = sum(1, 2);
+
+    expect(res).toBe(3)
+})
